339dc78928fec35deaa1c2e0ba22778f543aab30
Lynn Luong <lynn.h.luong@gmail.com>
2023-02-15 02:51:40 -0500

    testing: server working

--- authenticate-me/backend/app.js
+++ authenticate-me/backend/app.js
@@ -4,10 +4,13 @@ const cors = require('cors');
 const csurf = require('csurf');
 const helmet = require('helmet');
 const cookieParser = require('cookie-parser');
-
+const routes = require('./routes');
 const { environment } = require('./config');
+
 const isProduction = environment === 'production';
+
 const app = express();
+
 app.use(morgan('dev'));
 app.use(cookieParser());
 app.use(express.json());
@@ -16,22 +19,27 @@ app.use(express.json());
 if (!isProduction) {
     // enable cors only in development
     app.use(cors());
-  }
+}
 
-  // helmet helps set a variety of headers to better secure your app
-  app.use(
+// helmet helps set a variety of headers to better secure app
+app.use(
     helmet.crossOriginResourcePolicy({
-      policy: "cross-origin"
+        policy: "cross-origin"
     })
-  );
+);
 
-  // Set the _csrf token and create req.csrfToken method
-  app.use(
+// Setting the _csrf token and create req.csrfToken method
+app.use(
     csurf({
-      cookie: {
-        secure: isProduction,
-        sameSite: isProduction && "Lax",
-        httpOnly: true
-      }
+        cookie: {
+            secure: isProduction,
+            sameSite: isProduction && "Lax",
+            httpOnly: true
+        }
     })
-  );
+);
+
+app.use(routes);
+
+
+module.exports = app;
--- authenticate-me/backend/config/index.js
+++ authenticate-me/backend/config/index.js
@@ -1,8 +1,8 @@
-//Config file that will read environment variables loaded and export them as a key 
+//Config file that will read environment variables loaded and export them as a key
 
 module.exports = {
     environment: process.env.NODE_ENV || 'development',
-    port: process.env.PORT || 5000,
+    port: process.env.PORT || 5001,
     db: {
       username: process.env.DB_USERNAME,
       password: process.env.DB_PASSWORD,
