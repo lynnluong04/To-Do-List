422d428c70b9f4adf6a99ff0f7fd8f6f3ea101b0
Lynn Luong <lynn.h.luong@gmail.com>
2023-02-17 00:51:37 -0500

    login form

---
+++ authenticate-me/frontend/src/components/LoginFormPage/index.js
@@ -0,0 +1,60 @@
+import React, { useState } from 'react';
+import * as sessionActions from '../../store/session';
+import { useDispatch, useSelector } from 'react-redux';
+import { Redirect } from 'react-router-dom';
+import './LoginForm.css';
+
+
+function LoginFormPage() {
+    const dispatch = useDispatch()
+    const sessionUser = useSelector(state => state.session.user)
+    const [credential, setCredential] = useState('')
+    const [password, setPassword] = useState('')
+    const [errors, setErrors] = useState([])
+
+    if (sessionUser) return (
+        <Redirect to="/" />
+    );
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        setErrors([]);
+        return dispatch(sessionActions.login({ credential, password }))
+            .catch(async (res) => {
+                const data = await res.json();
+                if (data && data.errors) setErrors(data.errors);
+            });
+    }
+
+    return (
+        <div className='login form container'>
+            <form className='login' onSubmit={handleSubmit}>
+                <div> Log in to your account</div>
+                <ul>
+                    {errors.map((error, idx) => <li key={idx}>{error}</li>)}
+                </ul>
+                <label>
+                    Username or Email
+                </label>
+                <input
+                    type="text"
+                    value={credential}
+                    onChange={(e) => setCredential(e.target.value)}
+                    required
+                />
+                <label>
+                    Password
+                </label>
+                <input
+                    type="password"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                    required
+                />
+                <button type="submit">Log In</button>
+            </form>
+        </div>
+    );
+}
+
+export default LoginFormPage;
